4bdb9e132c45a43aec0987b197eec3e7d400ade8
Naazar <nazzik173@gmail.com>
2026-01-13 13:37:01 +0200

    Fix Selenium CI: use built-in ChromeDriver management

--- tests/ui/conftest.py
+++ tests/ui/conftest.py
@@ -1,12 +1,11 @@
-import pytest
-from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-
+from selenium import webdriver
 
 @pytest.fixture
 def driver():
     chrome_options = Options()
-    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
 
